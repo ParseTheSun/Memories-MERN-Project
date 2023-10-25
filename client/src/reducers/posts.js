9a99799be487dbd6cf689d74e25dada90a00f832
Dejan Vucic <devjan.vucic@gmail.com>
2023-10-25 22:24:02 +0200

    first commit - commiting all files to repository

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,15 @@
+const postsReducer = (posts = [], action) => {
+	switch (action.type) {
+			case 'FETCH_ALL':
+				return action.payload
+			case 'CREATE':
+				return [...posts, action.payload]
+			case 'UPDATE':
+				return posts.map((post) => post._id === action.payload._id ? action.payload : post )
+			case 'DELETE':
+				return posts.filter((post) => action.payload !== post._id)
+			default:
+				return posts;
+		}
+}
+export default postsReducer
---
+++ server/controllers/posts.js
@@ -0,0 +1,49 @@
+import PostMessage from '../models/postMessage.js'
+import mongoose from "mongoose";
+
+export const getPosts = async (req, res)=>{
+	try{
+		const postMessages = await PostMessage.find();
+
+		res.status(200).json(postMessages)
+
+	} catch(error){
+		res.status(404).json({ message : error.message })
+	}
+}
+
+export const createPost = async (req, res) => {
+	const post = req.body
+
+	const newPost = new PostMessage(post)
+
+	try{
+		await newPost.save()
+
+		res.status(201).json(newPost)
+
+	} catch(error){
+		res.status(409).json( {message: error.message} )
+	}
+}
+
+export const updatePost = async (req, res) => {
+
+		const post = req.body
+		const { id } = req.params
+
+		if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that id")
+
+		const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true })
+		res.status(200).json(updatedPost)
+}
+
+export const deletePost = async(req, res) => {
+	const id = req.params.id
+	
+	if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("Post doesn't exist or has already beed delted")
+	
+	await PostMessage.findByIdAndDelete(id)
+	
+	res.status(200).json({ message: "Post deleted successfully" })
+}
